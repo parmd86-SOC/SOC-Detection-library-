2124a562e86fa27f49572717a20de7e9eb318606
parmd86-SOC <parmd86@gmail.com>
2026-01-26 17:31:17 +0400

    Update route.ts

--- app/api/use-cases/[id]/route.ts
+++ app/api/use-cases/[id]/route.ts
@@ -1,5 +1,3 @@
-export const dynamic = 'force-dynamic'
-export const runtime = 'nodejs'
 import { NextRequest, NextResponse } from 'next/server'
 import { prisma } from '@/lib/prisma'
 import { updateUseCaseSchema } from '@/lib/validations'
