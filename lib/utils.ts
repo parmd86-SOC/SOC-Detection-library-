6f66adf103ee1da81a35ca1187987f4a7716ea19
parmd86-SOC <parmd86@gmail.com>
2026-01-27 21:22:01 +0400

    Update utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -22,3 +22,22 @@ export function formatDateTime(date: Date | string): string {
     minute: '2-digit',
   })
 }
+
+export function debounce<T extends (...args: any[]) => any>(
+  func: T,
+  wait: number
+): (...args: Parameters<T>) => void {
+  let timeout: NodeJS.Timeout | null = null
+
+  return function executedFunction(...args: Parameters<T>) {
+    const later = () => {
+      timeout = null
+      func(...args)
+    }
+
+    if (timeout) {
+      clearTimeout(timeout)
+    }
+    timeout = setTimeout(later, wait)
+  }
+}
